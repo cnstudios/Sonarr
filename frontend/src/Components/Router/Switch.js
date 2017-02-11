56dab5851f7883ab20e5ee79aa681531b47bdad1
Mark McDowall <markus.mcd5@gmail.com>
2017-02-10 22:47:11 -0800

    v3 UI

---
+++ frontend/src/Components/Router/Switch.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react';
+import { Switch as RouterSwitch } from 'react-router-dom';
+import getPathWithUrlBase from 'Utilities/getPathWithUrlBase';
+import { map } from 'Helpers/elementChildren';
+
+class Switch extends Component {
+
+  //
+  // Render
+
+  render() {
+    const {
+      children
+    } = this.props;
+
+    return (
+      <RouterSwitch>
+        {
+          map(children, (child) => {
+            const {
+              path: childPath,
+              addUrlBase = true
+            } = child.props;
+
+            if (!childPath) {
+              return child;
+            }
+
+            const path = addUrlBase ? getPathWithUrlBase(childPath) : childPath;
+
+            return React.cloneElement(child, { path });
+          })
+        }
+      </RouterSwitch>
+    );
+  }
+}
+
+Switch.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Switch;
